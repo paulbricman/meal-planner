d3dd3292e4badacbc7d9ae3955dba934cdf6296f
Paul Bricman <paulbricman@protonmail.com>
2022-02-05 14:00:30 +0100

    feat: change render

--- main.py
+++ main.py
@@ -25,20 +25,11 @@ if cols[0].button('generate new plan'):
     st.experimental_rerun()
 
 st.markdown('---')
-cols = st.columns(2)
 meals = ['breakfast', 'morning snack', 'lunch', 'afternoon snack', 'dinner']
 plan = generate_prune_plan('1x1')
 
 for meal in meals:
-    cols[0].markdown('##### ' + plan[0][meal][0])
-    cols[0].markdown(', '.join(plan[0][meal][1]))
-    cols[0].markdown('')
-
-cols[1].markdown('#### 🛒 shopping list')
-all_ingredients = [plan[0][meal][1] for meal in meals]
-all_ingredients = set(
-    [elem for sublist in all_ingredients for elem in sublist])
-all_ingredients = '\n'.join(['- [ ] ' + e for e in all_ingredients])
-cols[1].text(all_ingredients)
-
-st.markdown('---')
+    st.markdown('##### ' + plan[0][meal][0])
+    for ingredient in plan[0][meal][1]:
+        st.text('- [ ] ' + ingredient)
+    st.markdown('')
